c8082c4014c1d48c63bd86cc315feff755a7ce30
faparipa <farsang.gabor@gmail.com>
2025-07-15 18:11:02 +0200

    project completed

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: 'https://wondrous-anchovy-17.clerk.accounts.dev/',
+      applicationID: 'convex',
+    },
+  ],
+};
---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,6 @@
+import StreamVideoProvider from '@/components/providers/StreamClientProvider';
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return <StreamVideoProvider>{children}</StreamVideoProvider>;
+}
+export default Layout;
